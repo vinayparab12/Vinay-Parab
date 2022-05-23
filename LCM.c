8de253ada2892e421f45e5d411d53a33287f46aa
Vinay Rajaram Parab <86846642+vinayparab12@users.noreply.github.com>
2022-05-23 20:51:08 +0530

    Create LCM.c

---
+++ LCM.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int lcm(int a , int b){
+static int m = 0;
+m = m + b;
+if(m % a == 0 && m % b == 0){
+
+ return m;
+}
+
+return lcm(a , b);
+}
+
+int main(){
+int x , y;
+printf("Enter First Number : ");
+scanf("%d" , &x);
+printf("Enter Seconf Number : ");
+scanf("%d" , &y);
+printf("LCM of %d and %d = %d" , x , y , lcm(x , y));
+return 0;
+}
